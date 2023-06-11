421fa1f96d5e8ac904d82b0c941948fabc08ed60
Inuzuka Tomohiro <tomohiro.inuzuka@arsaga.jp>
2023-06-11 15:22:30 +0900

    [Add]React追加なんかAPIしようって感じじゃない。

---
+++ src/app/Http/Controllers/ListController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListController extends Controller
+{
+    public function index()
+    {
+        return response()->json(
+            [
+                "post" => [
+                    [
+                        "id" => 1,
+                        "title" => "タイトルです",
+                        "content" => "投稿内容です投稿内容です投稿内容です投稿内容です投稿内容です。"
+                    ],
+                    [
+                        "id" => 2,
+                        "title" => "タイトルです",
+                        "content" => "投稿内容です投稿内容です投稿内容です投稿内容です投稿内容です。"
+                    ],
+                    [
+                        "id" => 3,
+                        "title" => "タイトルです",
+                        "content" => "投稿内容です投稿内容です投稿内容です投稿内容です投稿内容です。"
+                    ],
+                ]
+            ],
+            200,[],
+            JSON_UNESCAPED_UNICODE //文字化け対策
+        );
+    }
+}
--- src/vite.config.js
+++ src/vite.config.js
@@ -1,10 +1,33 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import react from '@vitejs/plugin-react';
+//
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: [
+//                 'resources/sass/app.scss',
+//                 'resources/js/app.js',
+//             ],
+//             refresh: true,
+//         }),
+//         react(),
+//     ],
+// });
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
+        react(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/sass/app.scss',
+                // 'resources/ts/index.tsx'
+            ],
             refresh: true,
         }),
     ],
