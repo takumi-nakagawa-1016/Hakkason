76654e5fb997a4529c0e13a2c4c1cd8b3b3e4013
takumi-nakagawa-1016 <takumi.nakagawa@arsaga.jp>
2023-06-13 17:01:02 +0900

    viteが反応するように修正

--- src/vite.config.js
+++ src/vite.config.js
@@ -8,4 +8,13 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
 });
